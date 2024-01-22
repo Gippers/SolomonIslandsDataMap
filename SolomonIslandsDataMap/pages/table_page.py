16594bb25d570aa63d7b0d785890f07fc2fc4d24
Thomas Martin <thomasm851@gmail.com>
2024-01-22 15:57:11 +1100

    Created a population dash grid

--- SolomonIslandsDataMap/pages/table_page.py
+++ SolomonIslandsDataMap/pages/table_page.py
@@ -8,12 +8,12 @@ __all__ = ['title', 'init_init', 'layout', 'update_grid', 'update_page_rows', 'u
 from nbdev.showdoc import *
 # TODO work out how to get around below hack
 try:
-    from SolomonIslandsDataMap.dash_components import gen_census_grid, gen_dd
+    from SolomonIslandsDataMap.dash_components import gen_census_grid, gen_pop_grid, gen_dd
     from SolomonIslandsDataMap.app_data import mytitle, data_grid, stored_data, dropdown_location \
         , control_type, dd_var, dd_measure, dropdown_geo, download_button
     from SolomonIslandsDataMap.load_data import SolomonGeo
 except: 
-    from dash_components import gen_census_grid, gen_dd
+    from dash_components import gen_census_grid, gen_pop_grid, gen_dd
     from app_data import mytitle, data_grid, stored_data, dropdown_location \
         , control_type, dd_var, dd_measure, dropdown_geo, download_button
     from load_data import SolomonGeo
